90ec1f403a551c2fca234e3ee1e5c357f5a8467d
matchbestsoftware1033-lgtm <matchbestsoftware1033@gmail.com>
2026-02-07 17:43:30 +0530

    fix-tip-top

--- src/components/TiptapEditor.jsx
+++ src/components/TiptapEditor.jsx
@@ -8,13 +8,13 @@ import Underline from '@tiptap/extension-underline';
 import TextAlign from '@tiptap/extension-text-align';
 import Placeholder from '@tiptap/extension-placeholder';
 // Table extensions
-import Table from '@tiptap/extension-table';
+import {Table} from '@tiptap/extension-table';
 import TableRow from '@tiptap/extension-table-row';
 import TableHeader from '@tiptap/extension-table-header';
 import TableCell from '@tiptap/extension-table-cell';
 // Text styling
 import { Color } from '@tiptap/extension-color';
-import TextStyle from '@tiptap/extension-text-style';
+import {TextStyle} from '@tiptap/extension-text-style';
 import Highlight from '@tiptap/extension-highlight';
 // Task list
 import TaskList from '@tiptap/extension-task-list';
